a5b1242f330d85c202b129e93d9548aec3b57267
Nadine <nadine_paez@hotmail.com>
2021-12-19 18:14:51 -0500

    thought routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
-const { addThought, removeThought } = require('../../controllers/thought-controller');
+const { 
+    getAllThoughts, 
+    getThoughtById,
+    updateThought, 
+    addThought, 
+    removeThought } = require('../../controllers/thought-controller');
 
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought);
 
 // /api/thoughts/<userId>/<thoughtId>
 router.route('/:userId/:thoughtId').delete(removeThought);
+
+// get all thoughts
+router.route('/').get(getAllThoughts);
+
+//get thoughts by id
+router
+.route('/:id')
+.get(getThoughtById)
+.put(updateThought);
 
 module.exports = router;
